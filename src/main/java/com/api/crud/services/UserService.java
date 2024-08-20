7bc1c7d436f717bce384903ab2b9446a5f4956f7
Gerald2309 <gerald.e2309@gmail.com>
2024-08-20 18:55:37 -0500

    Done

--- src/main/java/com/api/crud/services/UserService.java
+++ src/main/java/com/api/crud/services/UserService.java
@@ -1,4 +1,48 @@
 package com.api.crud.services;
 
+import com.api.crud.models.UserModel;
+import com.api.crud.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    IUserRepository userRepository;
+
+    public ArrayList<UserModel> getUsers() {
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
+
+    public UserModel saveUser(UserModel user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<UserModel> getById(Long id) {
+        return userRepository.findById(id);
+
+    }
+
+    public Optional<UserModel> update(Long id, UserModel user) {
+        return userRepository.findById(id).map(u -> {
+            u.setFirstName(user.getFirstName());
+            u.setLastName(user.getLastName());
+            u.setEmail(user.getEmail());
+            return userRepository.save(u);
+        });
+    }
+
+    public Boolean deleteUser(Long id){
+        try {
+            userRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
